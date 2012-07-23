2de193ad4f304fda41db464e195acfe0c4b0cc4e
Tomer Shvueli <tomer@shvueli.com>
2012-07-23 17:05:41 -0400

    Updated the static HTML slightly, worked on making the RoomReservation model and view work together.

--- js/RoomReservation.js
+++ js/RoomReservation.js
@@ -3,7 +3,8 @@ $(function() {
 	// The single RoomReservation model
 	var RoomReservation = Backbone.Model.extend({
 		defaults: {
-			name: 'Add A Room'
+			name:  'Add A Room',
+			state: 'shown',
 		}
 	});
 
@@ -17,19 +18,42 @@ $(function() {
 		tagName:   "fieldset",
 		className: "room-wrap",
 		template: $("#roomres-template").html(),
-		render: function() {
+		events: {
+			"click .room-names input" : "updateName"
+		},
+		initialize: function() {
+			_.bindAll(this, "toggleState");
+			this.model.bind('change:state', this.toggleState);
+		},
+		render: function(model) {
 			var templ = _.template(this.template);
 			this.$el.html(templ(this.model.toJSON()));
-			console.log('room view rendered');
+			if (this.model.get("state") == "pre-click") {
+				this.$el.addClass("add");
+			}
+			console.log('new room view rendered');
 			return this;
+		},
+		updateName: function(e) {
+			this.model.set("name", e.currentTarget.defaultValue);
+			console.log(this.model.get("name"));
+		},
+		toggleState: function() {
+			console.log('toggle');
+			if (this.model.get("shown") == true) {
+				$(this.el).find(".room").slideUp();
+			} else {
+				$(this.el).find(".room").slideDown();
+			}
 		}
 	});
 
 	var AppView = Backbone.View.extend({
 		el: $("#room-reservations"),
 		events: {
-			"click .room-wrap.add" : "renderRoom",
-			"click .delete" : "removeReservation"
+			"click #add" : "addRoom",
+			"click .delete"        : "removeReservation",
+			"click .toggle" : "toggleModel"
 		},
 		initialize: function() {
 			this.roomsCollection = new Rooms();
@@ -40,25 +64,37 @@ $(function() {
 			that.renderRoom();
 		},
 		renderRoom: function() {
-			var room     = new RoomReservation();
+			var room     = new RoomReservation;
 			var roomView = new RoomReservationView({
-				model: room
+				model: room,
+				id: "reservation-" + this.roomsCollection.length
 			});
 			this.roomsCollection.add(roomView);
-			this.$el.append(roomView.render().el);			
+			this.$el.append(roomView.render().el);
 		},
-		removeReservation: function() {
+		addRoom: function() {
+
+		},
+		removeReservation: function(e) {
 			if (this.roomsCollection.length == 1) {
-				console.log("can't remove a single reservation.");
+				console.log("can't remove a single reservation");
 			} else {
-				//this.model.destroy();
-				this.remove();
-				console.log('remove');
+				var id  = $(e.currentTarget).parents('.room-wrap').attr('id');
+				var obj = this.roomsCollection.get(id);
+				this.roomsCollection.remove(obj);
 			}
+		},
+		toggleModel: function(e) {
+			var id = $(e.currentTarget).parent().attr('id');
+			var obj = this.roomsCollection.get(id);
+			console.log(obj.get("shown"));
+			obj.set("state", !obj.get("shown"));
+			console.log(obj.get("state"))
 		}
 	});
 
 	var App = new AppView();
+
 
 });
 
