cdf173933198d9a89e15b661e68290deeb0904fc
mahesh <mahesh@mahesh.(none)>
2012-09-25 22:03:14 -0400

    Completed implementation of remaining html fields in room request form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 	window.app = AppController.init({});
+        $("#event-form").validate();
+  
+});
+
+
+$("input[name='catering-type']").click(function() {
+	if($(this).val()=="catering-food")
+	{
+    		$("#catering-details").show();
+  	}
+	else
+  	{
+		$("#catering-details").hide();
+  	}
 });
